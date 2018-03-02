2df24c97e586e73c4aa520b1cb79cb28a69e8947
Oyesola Ogundele <oyekunmi@gmail.com>
2018-03-02 18:46:09 +0100

    Changing colors

--- script.js
+++ script.js
@@ -3,13 +3,25 @@
 
     const root = document.documentElement;
     const range = document.querySelector('.booth-slider');
+    let inputs = root.querySelectorAll('.color-box > input');
 
     //as slider range's value changes, do something 
     range.addEventListener('input', handleSlider)
 
+    inputs.forEach(element => {
+        element.addEventListener('input', handleColor);
+    });
+
     function handleSlider (e) {
         let value = e.target.value 
         root.style.setProperty('--slider', value)
+    }
+
+    function handleColor(e){
+        let value = e.target.value;
+        let parentId = e.target.parentNode.id 
+        let inputBg = `--bg-${parentId}` 
+        root.style.setProperty(inputBg, value)
     }
 
 
